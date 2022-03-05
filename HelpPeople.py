0e03b3e06b81e997e0f8f1f8b5728d430265e5e7
Torsten Diesel <torsten@thediesels.net>
2022-03-05 23:25:15 +0000

    Started conversion from pseudocode to python

---
+++ HelpPeople.py
@@ -0,0 +1,19 @@
+def main_menu():
+	notdecided == true
+	while notdecided == true:
+		print("where do you want to go?")
+		print("options:")
+	
+		let option = str(input("> "))
+		if option == "a":
+			notdecided = false
+			do this
+		if option == b:
+			notdecided = false
+			do something else
+		if option == c:
+			notdecided = false
+			do something something else
+		else:
+			print try again!
+			// should loop automatically because of the notdecided while loop
