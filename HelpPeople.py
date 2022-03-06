ed4614195333713fc4cf3306e36cdeaf05a2c5c7
thunderblood101 <torsten@thediesels.net>
2022-03-06 09:45:52 -0500

    Updated menu (doesn't work yet)

--- HelpPeople.py
+++ HelpPeople.py
@@ -3,17 +3,26 @@ def main_menu():
 	while notdecided == true:
 		print("where do you want to go?")
 		print("options:")
+		print("\ta. Import the data as a csv.")
+		print("\tb. Dump all data and leave you to process it."
+		print("\tc. Analyze and interpret the data.")
 	
-		let option = str(input("> "))
+		let option = str(input("(lowercase letter) > "))
 		if option == "a":
 			notdecided = false
-			do this
-		if option == b:
+			import_it()
+		if option == "b":
 			notdecided = false
-			do something else
-		if option == c:
+			dump()
+		if option == "c":
 			notdecided = false
-			do something something else
+			kowalski_analysis()
 		else:
-			print try again!
+			print("try again!")
 			// should loop automatically because of the notdecided while loop
+			  
+def import_it():
+	// TODO
+			  
+def main():
+	main_menu()
